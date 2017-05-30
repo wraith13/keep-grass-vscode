60bca9d6884437a289ac82ea8090388dcfb94b73
道化師 <wraith@trickpalace.net>
2017-05-31 01:44:19 +0900

    🛠 WIP

--- src/extension.ts
+++ src/extension.ts
@@ -46,7 +46,8 @@ module rx {
     }
 }
 
-module KeepGrass {
+export module KeepGrass
+{
     function getConfiguration<type>(key?: string): type
     {
         var configuration = vscode.workspace.getConfiguration("keep-grass-vscode");
@@ -57,6 +58,8 @@ module KeepGrass {
 
     export function registerCommand(context: vscode.ExtensionContext): void
     {
+        var statusBarItem = new StatusBarItem();
+        context.subscriptions.push(statusBarItem);
         context.subscriptions.push
         (
             vscode.commands.registerCommand
@@ -64,6 +67,7 @@ module KeepGrass {
                 'keep-grass-vscode.update', update
             )
         );
+        statusBarItem.updateWordCount();
     }
 
     export async function update() : Promise<void>
@@ -79,11 +83,110 @@ module KeepGrass {
             else
             if (response.statusCode === 200)
             {
-                console.log(body);
+                //console.log(body);
+                var lastUpdate = getLastUpdate(body);
+                console.log(lastUpdate);
+                if (lastUpdate)
+                {
+                    console.log(lastUpdate.toLocaleString());
+                }
             }
         }
     }
 
+    function parseISODate(source : string) : Date
+    {
+        return new Date(Date.parse(source.replace("T", " ")));
+    }
+
+    function getLastUpdate(xml : string) : Date | null
+    {
+        var match = /<updated>(.*?)<\/updated>/.exec(xml);
+        //console.log(match ? `${match[1]} ${match[2]}`: null);
+        return match ? new Date(parseISODate(match[1])): null;
+    }
+
+    export function numberToByteString(value : number) : string
+    {
+        if (value <= 0.0)
+        {
+            return "00";
+        }
+        if (1.0 <= value)
+        {
+            return "ff";
+        }
+        return ("00" +Math.floor(value *255).toString(16)).substr(-2);
+    }
+    export function MakeLeftTimeColor(LeftTimeRate : number) : string
+    {
+        return numberToByteString(1.0 - LeftTimeRate)
+            + numberToByteString(Math.min(0.5, LeftTimeRate))
+            + numberToByteString(0.0);
+    }
+
+    class StatusBarItem
+    {
+
+        private _statusBarItem: vscode.StatusBarItem;
+
+        public updateWordCount()
+        {
+
+            // Create as needed
+            if (!this._statusBarItem)
+            {
+                this._statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+            }
+
+            this._statusBarItem.text = "🌱！！！！";
+            this._statusBarItem.color = "#FF8888";
+            this._statusBarItem.show();
+
+            /*
+            // Get the current text editor
+            let editor = vscode.window.activeTextEditor;
+            if (!editor) {
+                this._statusBarItem.hide();
+                return;
+            }
+
+            let doc = editor.document;
+
+            // Only update status if an Markdown file
+            if (doc.languageId === "markdown") {
+                let wordCount = this._getWordCount(doc);
+
+                // Update the status bar
+                this._statusBarItem.text = wordCount !== 1 ? `${wordCount} Words` : '1 Word';
+                this._statusBarItem.show();
+            } else {
+                this._statusBarItem.hide();
+            }*/
+        }
+
+        public _getWordCount(doc: vscode.TextDocument): number
+        {
+
+            let docContent = doc.getText();
+
+            // Parse out unwanted whitespace so the split is accurate
+            docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
+            docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+            let wordCount = 0;
+            if (docContent !== "")
+            {
+                wordCount = docContent.split(" ").length;
+            }
+
+            return wordCount;
+        }
+
+        dispose()
+        {
+            this._statusBarItem.dispose();
+        }
+    }
 }
 
 export function activate(context: vscode.ExtensionContext) {
